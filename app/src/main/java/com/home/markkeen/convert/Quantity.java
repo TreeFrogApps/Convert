03630c3b65747131af3f5281a967959ea7a44a30
Mark Keen <markchugnut@ntlworld.com>
2015-04-26 18:51:09 +0100

    main commit

--- app/src/main/java/com/home/markkeen/convert/Quantity.java
+++ app/src/main/java/com/home/markkeen/convert/Quantity.java
@@ -6,7 +6,7 @@ public class Quantity {
     final double value;
     final Unit unit;
 
-    public static enum Unit {
+    public enum Unit {
 
         Kilometer(1.0d), Meter(1000.0d), Centimeter(100000.0d), Millimeter(1000000.0d),
         Mile(0.6214d), Yard(1093.61d), Foot(3280.84d), Inch(39370.1d), NauticalMile(0.54d),
@@ -34,7 +34,7 @@ public class Quantity {
 
         final double baseUnit;
 
-        private Unit(double inBaseUnit){
+        Unit(double inBaseUnit){
             this.baseUnit = inBaseUnit;
         }
 
